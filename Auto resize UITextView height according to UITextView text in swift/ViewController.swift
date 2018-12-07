326dde3935c2d6b92a2f383d848c12b0c990048f
Arthur <arthur@Arthurs-MacBook-Air.local>
2018-12-07 16:30:40 +0300

    Initial Commit

---
+++ Auto resize UITextView height according to UITextView text in swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto resize UITextView height according to UITextView text in swift
+//
+//  Created by Arthur on 07/12/2018.
+//  Copyright © 2018 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
